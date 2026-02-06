8edca6b8a33e852d80a6c68353c11c6c50b1869d
Hashmi <patanhashmi20@gmail.com>
2026-02-06 21:42:24 +0530

    responsive&home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { GignupComponent } from './gignup/gignup.component';
 import { AboutComponent } from './about/about.component';
+import { DestinationsComponent } from './destinations/destinations.component';
 export const routes: Routes = [
     {path:'',component:HomeComponent  },
     { path: 'signin', component: SigninComponent},
     { path: 'gignup', component: GignupComponent } ,
-  { path: 'About', component: AboutComponent },
+    { path: 'About', component: AboutComponent },
+    { path: 'destinations', component: DestinationsComponent }
 
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
